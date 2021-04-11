7a16188a8be62f5e4adf5a5c982c974ff0498e38
AppDeEventos <eventosdeapp@gmail.com>
2021-04-10 21:42:23 -0300

    Agregamos el boton de Registrarse que lleva al formulario de registro

--- app/src/main/java/com/fedemcode/uruguaysevacuna/LoginActivity.java
+++ app/src/main/java/com/fedemcode/uruguaysevacuna/LoginActivity.java
@@ -2,13 +2,28 @@ package com.fedemcode.uruguaysevacuna;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button botonnoregistro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        botonnoregistro=(Button)findViewById(R.id.botonnoregistro);
+
+        botonnoregistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i=new Intent(LoginActivity.this,registro.class);
+                startActivity(i);
+            }
+        });
     }
 }
